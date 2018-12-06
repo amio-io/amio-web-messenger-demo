e234d766ece8ad653c755350dd2b67a443a7e994
Matouš Kučera <matous.kucera@gmail.com>
2018-12-06 15:16:11 +0100

    feat: initial version

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="chat">
+    <div>
+      <div>
+        <h3>Chat Group</h3>
+        <hr>
+      </div>
+      <div>
+        <div class="messages" v-for="(msg, index) in messages" :key="index">
+          <p><span>{{ msg.user }}: </span>{{ msg.message }}</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <form @submit.prevent="sendMessage">
+        <div>
+          <label for="user">User:</label>
+          <input type="text" v-model="user">
+        </div>
+        <div>
+          <label for="message">Message:</label>
+          <input type="text" v-model="message">
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  import io from 'socket.io-client'
+
+  export default {
+    data() {
+      return {
+        user: '',
+        message: '',
+        messages: [],
+        socket: null,
+        sessionId: localStorage.getItem('amio_chat_session')
+      }
+    },
+    methods: {
+      sendMessage(e) {
+        e.preventDefault()
+
+        this.messages.push({
+          user: this.user,
+          message: this.message
+        })
+
+        this.socket.emit('message_client', {
+          channel: {
+            id: '12345'
+          },
+          contact: {
+            id: this.sessionId,
+            name: this.user
+          },
+          content: {
+            type: 'text',
+            payload: this.message
+          }
+        })
+        this.message = ''
+      }
+    },
+    mounted() {
+      const opts = {
+        reconnection: true,
+        reconnectionDelay: 1000,
+        reconnectionDelayMax: 5000,
+        reconnectionAttempts: 99999
+      }
+      if(this.sessionId) {
+        opts.query = {sessionId: this.sessionId}
+      }
+      this.socket = io('localhost:8888', opts)
+
+      this.socket.on('message_server', (data, ack) => {
+        this.messages.push({
+          user: data.contact.id,
+          message: data.content.payload
+        })
+        ack('ok')
+      })
+
+      this.socket.on('connection_accepted', data => {
+        const sessionId = data.session_id
+        this.sessionId = sessionId
+        localStorage.setItem('amio_chat_session', data.session_id)
+      })
+    }
+  }
+</script>
+
+<style>
+#chat {
+  text-align: left;
+}
+</style>
