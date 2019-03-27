8149b0124a71e39135fc96f8a65b9d0ab37753c6
Karel Rochelt <karel.rochelt@amio.io>
2019-03-27 14:15:08 +0100

    feat: adapted to new webchat sdk api & added some more features

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,6 +33,7 @@
         <button type="button" @click="markMessagesAsRead">Mark as read</button>
         <button type="button" @click="loadHistory">Load history</button>
         <button type="button" @click="sendNotification">Send test notification</button>
+        <button type="button" @click="sendImage">Send image</button>
       </form>
     </div>
   </div>
@@ -68,7 +69,7 @@
         }
         this.messages.push(sentMessage)
 
-        this.chatClient.sendTextMessage(this.message)
+        this.chatClient.messages.sendText(this.message)
           .then((response) => {
             //update the message with extra data from response (delivered timestamp etc.)
             Object.assign(sentMessage, response)
@@ -76,8 +77,11 @@
           
         this.message = ''
       },
+      sendImage() {
+        this.chatClient.messages.sendImage('https://www.todaysparent.com/wp-content/uploads/2017/06/when-your-kid-becomes-a-meme-1024x576-1497986561.jpg')
+      },
       sendNotification() {
-        this.chatClient.sendNotification({test: 'hello'})
+        this.chatClient.notifications.send({test: 'hello'})
       },
       receiveNotification(data) {
         this.messages.push({content: {payload: `Notification: ${JSON.stringify(data)}`}})
@@ -86,10 +90,10 @@
         this.messages.push(data)
       },
       markMessagesAsRead() {
-        this.chatClient.markMessagesAsRead()
+        this.chatClient.notifications.sendMessagesRead()
       },
       loadHistory() {
-        this.chatClient.listMessages(this.historyCount, this.historyCursor)
+        this.chatClient.messages.list(this.historyCursor, this.historyCount)
           .then(this.historyLoaded)
       },
       historyLoaded(history) {
@@ -110,12 +114,14 @@
       if(process.env.VUE_APP_AMIO_WEBCHAT_SERVER_URL) {
         config._amioWebchatServerUrl = process.env.VUE_APP_AMIO_WEBCHAT_SERVER_URL
       }
-      this.chatClient.connect(config)
-      this.chatClient.onMessageReceived(this.receiveMessage)
-      this.chatClient.onMessageEcho(this.receiveMessage)
-      this.chatClient.onConnectionStateChanged(this.setOnline)
-      this.chatClient.onNotificationReceived(this.receiveNotification)
-      this.loadHistory()
+      this.chatClient.events.onMessageReceived(this.receiveMessage)
+      this.chatClient.events.onMessageEcho(this.receiveMessage)
+      this.chatClient.events.onConnectionStateChanged(this.setOnline)
+      this.chatClient.events.onNotificationReceived(this.receiveNotification)
+      this.chatClient.connect(config).then(() => {
+        this.loadHistory()
+      })
+      
     }
   }
 </script>
